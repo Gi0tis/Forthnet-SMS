02815c2b7880b1fabb92d5f22103c2a0856c59ca
Giotis <gi0tis@ath.forthnet.gr>
2014-04-21 01:30:34 +0300

    SMS history and sent notification

--- ForthnetSms.py
+++ ForthnetSms.py
@@ -17,7 +17,6 @@ br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 br.addheaders = [('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:28.0) Gecko/20100101 Firefox/28.0')]
 r = br.open('https://www-old.forthnet.gr/secure/webSMS/login.aspx?ReturnUrl=%2fsecure%2fwebsms%2fdefault.aspx&xss_mode=l')
 
-
 #Authentication
 br.select_form(nr=0)
 br.form['Username'] = username
@@ -31,9 +30,15 @@ print "Phone Number:"
 number = raw_input()
 br.form['txtTo'] = number
 
-print "Sms Text:"
+print "SMS Text:"
 message = raw_input()
 br.form['txtMessage'] = message
 
 br.submit()
+print "SMS sent."
 
+#History
+smsThisMonthPage = br.open ('https://www-old.forthnet.gr/secure/websms/History.aspx')
+smsHTML = smsThisMonthPage.read()
+index = smsHTML.find('/ 50')
+print "You have sent",smsHTML[(index-3):index],"out of 50 free SMS this month"
