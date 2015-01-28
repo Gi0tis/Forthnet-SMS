23e6fc08814a846a26417ada2cc7ffeade1f3674
git-gi0tis.ath.forthnet.gr <gi0tis@ath.forthnet.gr>
2015-01-29 00:13:31 +0200

    bugs

--- ForthnetSms.py
+++ ForthnetSms.py
@@ -10,12 +10,13 @@ password = getpass.getpass()
 
 #Browser
 br = mechanize.Browser()
+#br.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 cj = cookielib.LWPCookieJar()
 br.set_cookiejar(cj)
 br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 br.addheaders = [('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:28.0) Gecko/20100101 Firefox/28.0')]
-r = br.open('https://www-old.forthnet.gr/secure/webSMS/login.aspx?ReturnUrl=%2fsecure%2fwebsms%2fdefault.aspx&xss_mode=l')
+r = br.open('https://www-old.forthnet.gr/secure/websms/default.aspx')
 
 #Authentication
 br.select_form(nr=0)
