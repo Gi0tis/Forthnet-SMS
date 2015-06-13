a05a58c18776766925dcdaad205e28c0d56c2383
Giotis <gi0tis@ath.forthnet.gr>
2015-06-14 02:29:08 +0300

    Fix for SSL by Xarilaos (http://giot.is/forthnet-sms/#comment-202)

--- ForthnetSms.py
+++ ForthnetSms.py
@@ -1,7 +1,24 @@
 import mechanize
 import cookielib
 import getpass
+import ssl
+from ssl import PROTOCOL_SSLv23, PROTOCOL_SSLv3, CERT_NONE, SSLSocket
 
+
+def monkey_wrap_socket(sock, keyfile=None, certfile=None,
+	server_side=False, cert_reqs=CERT_NONE,
+	ssl_version=PROTOCOL_SSLv23, ca_certs=None,
+	do_handshake_on_connect=True,
+	suppress_ragged_eofs=True, ciphers=None):
+	ssl_version=ssl.PROTOCOL_TLSv1
+	return SSLSocket(sock, keyfile=keyfile, certfile=certfile,
+	server_side=server_side, cert_reqs=cert_reqs,
+	ssl_version=ssl_version, ca_certs=ca_certs,
+	do_handshake_on_connect=do_handshake_on_connect,
+	suppress_ragged_eofs=suppress_ragged_eofs,
+	ciphers=ciphers)
+
+ssl.wrap_socket = monkey_wrap_socket
 #User input
 print "What's your email?"
 username = raw_input()
@@ -15,7 +32,7 @@ cj = cookielib.LWPCookieJar()
 br.set_cookiejar(cj)
 br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-br.addheaders = [('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:28.0) Gecko/20100101 Firefox/28.0')]
+br.addheaders = [('User-agent', 'Mozilla/5.0')]
 r = br.open('https://www-old.forthnet.gr/secure/websms/default.aspx')
 
 #Authentication
